9a32e2b22a01a3872cc134da035c1ebbff323a33
YSSVirus <56813561+YSSVirus@users.noreply.github.com>
2022-02-25 10:28:00 -0800

    Update gtfobins_Automated_SUID-Checker.sh

--- Privlage_Escalation/Scripts/Bash/gtfobins_Automated_SUID-Checker/gtfobins_Automated_SUID-Checker.sh
+++ Privlage_Escalation/Scripts/Bash/gtfobins_Automated_SUID-Checker/gtfobins_Automated_SUID-Checker.sh
@@ -1,23 +1,10 @@
 #! /bin/bash
 clear
-program_name=$1
+program_name_list=$1
 Main_Function(){
         while IFS="" read -r p || [ -n "$p" ]
         do
-                bash $(pwd)'/gtfobins_Single_SUID-Checker.sh' $p
-        done < $program_name
-}
-Question(){
-        echo 'Do you have the gtfobins_Single_SUID-Checker.sh script? (make sure you launch this script from the same folder as the previously mentioned script.)'
-        read Dependencie_Answer
-}
-Question
-if [[ $Dependencie_Answer == yes ]] || [[ $Dependencie_Answer == yes ]] || [[ $Dependencie_Answer == Y ]] || [[ $Dependencie_Answer == y ]]; then
-        Main_Function
-elif [[ $Dependencie_Answer == No ]] || [[ $Dependencie_Answer == no ]] || [[ $Dependencie_Answer == N ]] || [[ $Dependencie_Answer == n ]]; then
-        script_url=
-    wget script_url
-    Main_Function
-else
-        Question
-fi                                                                                                                                                                                            
+            curl --connect-timeout 2 'https://gtfobins.github.io/gtfobins/'$p'/#suid' | grep 'SUID' && echo $p' has a SUID entry on gtfobins here is the link https://gtfobins.github.io/gtfobins/'$p'/#suid'
+        done < $program_name_list
+    }
+Main_Function                                                                                                                                                                                            

f62ba01aece6c5351f5a9a9b812be834d736ae5e
YSSVirus <56813561+YSSVirus@users.noreply.github.com>
2022-03-18 20:15:25 -0700

    updated http_get_post-bruteforce.sh
    fixed http_get_post-bruteforce.sh so that the help argument is taken better. I also added the help to the help message

--- Brute_forcing/http_get_post-bruteforce.sh
+++ Brute_forcing/http_get_post-bruteforce.sh
@@ -4,11 +4,10 @@
 #IT CAN CAUSE MASSIVE ISSUES
 
 #These are the scripts main variables DO NOT TOUCH
-max_var_count=16
+max_var_count=19
 min_var_count=5
 
-
-
+arg_number="$#"
 
 
 all_caps_false="TRUE"
@@ -171,12 +170,10 @@ encode_creds() {
 main_function() {
 	for arg in "--threads" "--target" "--user" "--pass" "--subdirectory" "--incorrect_text" "--login_paramater" "--encode_creds"; do
 		if [[ $first == $arg ]]; then
-			echo hi
 			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
 			stripped_next_arg=$second
 			$stripped_arg $stripped_next_arg
 		elif [[ $third == $arg ]]; then
-			echo bye
 			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
 			stripped_next_arg=$fourth
 			$stripped_arg $stripped_next_arg
@@ -207,86 +204,115 @@ main_function() {
 		else
 			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
 			stripped_next_arg=" "
-			echo "the argument: "$stripped_arg" is being set automaticly"
+			echo "the argument: "$stripped_arg" is being set automaticly to make thise easier for you :)"
+			echo""
 			$stripped_arg $stripped_next_arg
 		fi
 	done
 	hydra $thread_count -I $username $password $target http-post-form /"$sub_directory_var:username=$user_var&password=$password_var$log_in:$incorrect"
 
 }
-for all_variables in "$first" "$second" "$third" "$fourth" "$fifth" "$sixth" "$seventh" "$eighth" "$ninth" "$tenth" "eleventh" "twelvth" "thirteenth" "fourteenth" "fifthteenth" "sixteenth" "seventeenth" "eighteenth"; do
-	for help in "-h" "-H" "-help" "-Help" "-HELP" "-hh"; do
-		if [[ $all_variables = "$help" ]]; then
-			clear
-			echo ""
-			echo "Thank you for using my script 
-					- YSSVirus"
-			echo ""
-			echo "This is the help menu for the script"
-			echo "				Required Arguments
-					
-					--target
-						This argument is for the targets domain
-							Example: 
-								--target example.htb
-
-					--user
-						This argument is for either a username or a user_file
-							Example:
-								--user username
-								--user user_file.txt
-		
-					--pass 
-						This argument is for either a password or password_file
-							Example:
-								--pass password
-								--pass password.txt
-
-
-
-									Optional Arguments
-			
-					--subdirectory
-						This argument is for the log-in pages sub directory
-							Default:
-								--subdirectory login
-							Example:
-								the 'log-in' part of 'https://example.com/log-in'
-								--subdirectory log-in
-								--subdirectory hompage/other-page/log-in
-
-					--incorrect_text
-						This argument is for the text displayed when incorrect credentials are supplied
-							Default:
-								This script will try to automaticly detect the text that is given with incorrect credentials
-							Example:
-								--incorrect_text 'Wrong user or password'
-								--incorrect_text wrong
-
-					--login_paramater
-						This is a true or false whether or not to include a very common paramater named submit that is set to nothing... its blank.
-							Default:
-								--login_paramater TRUE
-								--login_paramater t
-							Example:
-								--login_paramater TRUE
-								--login_paramater t
-								--login_paramater FALSE
-								--login_paramater f
-					--encode_creds
-						This argument is a true or false whether or not to base 64 encode the payloads
-					"
-			echo ""
-			echo "Here are some Examples"
-			echo ""
-			examples
-			exit
-		fi
-	done
-done
-if [[ $# > $min_var_count || $# < $max_var_count ]]; then
-	main_function
-else
+help() {
 	clear
+	echo ""
+	echo "Thank you for using my script 
+			- YSSVirus"
+	echo ""
+	echo "This is the help menu for the script"
+	echo "		Required Arguments
+	
+	--target
+		This argument is for the targets domain
+			Example: 
+				--target example.htb
+
+			--user
+		This argument is for either a username or a user_file
+			Example:
+				--user username
+				--user user_file.txt
+		
+			--pass 
+		This argument is for either a password or password_file
+			Example:
+				--pass password
+				--pass password.txt
+
+
+
+			Optional Arguments
+			
+			-h
+				
+
+		--subdirectory
+			This argument is for the log-in pages sub directory
+				Default:
+					--subdirectory login
+				Example:
+					the 'log-in' part of 'https://example.com/log-in'
+					--subdirectory log-in
+					--subdirectory hompage/other-page/log-in
+
+		--incorrect_text
+			This argument is for the text displayed when incorrect credentials are supplied
+				Default:
+					This script will try to automaticly detect the text that is given with incorrect credentials
+				Example:
+					--incorrect_text 'Wrong user or password'
+					--incorrect_text wrong
+
+		--login_paramater
+			This is a true or false whether or not to include a very common paramater named submit that is set to nothing... its blank.
+				Default:
+					--login_paramater TRUE
+					--login_paramater t
+				Example:
+					--login_paramater TRUE
+					--login_paramater t
+					--login_paramater FALSE
+					--login_paramater f
+		--encode_creds
+			This argument is a true or false whether or not to base 64 encode the payloads
+				Default:
+					--login_paramater FALSE
+					--login_paramater f
+				Example:
+					--login_paramater TRUE
+					--login_paramater t
+					--login_paramater FALSE
+					--login_paramater f"
+	echo ""
+	echo""
+	echo ""
+	echo "Here are some Examples"
+	echo ""
 	examples
+}
+check_any_help() {
+	for all_variables in "$first" "$second" "$third" "$fourth" "$fifth" "$sixth" "$seventh" "$eighth" "$ninth" "$tenth" "$eleventh" "$twelvth" "$thirteenth" "$fourteenth" "$fifthteenth" "$sixteenth" "$seventeenth" "$eighteenth"; do
+		for help in "-h" "-H" "-help" "-Help" "-HELP" "-hh"; do
+			if [[ "$all_variables" = "$help" ]]; then
+				will_help="help"
+			else 
+				break 1
+			fi
+		done
+	done
+}
+check_any_help
+echo "$arg_number"
+if [[ "$will_help" = "help" ]]; then
+	help
+else
+	if [[ "$arg_number" -le "$min_var_count" ]]; then
+		examples
+		exit
+	elif [[ "$arg_number" -ge "$max_var_count" ]]; then
+		examples
+		exit
+	else
+		main_function
+		exit
+	fi
 fi

